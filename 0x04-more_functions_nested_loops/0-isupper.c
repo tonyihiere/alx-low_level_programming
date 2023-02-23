8f8c52697fe7b20bd9483060b9cde94d6af8e14c
Anthony I. Enakhifo <tonitech24@gmail.com>
2023-02-23 15:08:57 -0800

    #include holberton.h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-  * _isupper - Verify an uppercase letter 
-  * @x: The digit to be verified is 
-  * 
-  * Return: 1 for the upper case letter, 0 for everything else.
+  *_isupper - Verify an uppercase letter
+  *@x: The digit to be verified
+  *Return: 1 for the upper case letter, 0 for everything else.
   */
+
+
 int _isupper(int x)
 {
 	if (x >= 65 && x <= 90)
