c9c54ee944b34c8912eb9767224ed618b1bc5311
Anthony I. Enakhifo <tonitech24@gmail.com>
2023-02-22 19:53:04 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
-int main()
+
+int main(void)
 {
-	write("_putchar\n");
+	char *h = "_putchar";
+
+	while (*h)
+	{
+		_putchar(*h);
+		h++;
+	}
+	_putchar('\n');
 
 	return (0);
 }
