0dc4ce14a322bd45c5fe0889bbe128b3070b0090
Anthony I. Enakhifo <tonitech24@gmail.com>
2023-03-09 04:44:26 -0800

    print rev recurtision

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print rev recursion
+ * Return: always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
